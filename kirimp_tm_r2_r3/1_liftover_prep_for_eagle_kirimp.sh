ec2c6fc52247a3bd3b4b050545225c375eeff90c
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-08-27 21:51:07 -0600

    Reorder to subset to chr19 first

--- kirimp_tm_r2_r3/1_liftover_prep_for_eagle_kirimp.sh
+++ kirimp_tm_r2_r3/1_liftover_prep_for_eagle_kirimp.sh
@@ -4,7 +4,7 @@ if [ "$#" -eq 0 ]
 then
    echo "Usage: ${0##*/} <input_vcf> <crossmap_chain>"
    echo "       <ref_fasta> <output_dir>"
-   echo "Script uses CrossMap.py for liftover to hg19."
+   echo "Script uses CrossMap for liftover to hg19."
    echo "After liftover, removes any previous phasing"
    echo "and prepares for input into Eagle for phasing."
    exit
@@ -19,35 +19,33 @@ output_dir=$4
 # Get basename of input_vcf
 input_vcf_name=$(basename $input_vcf | sed 's/\.vcf.*//')
 
+# Pass through PLINK to select chr19 & remove previous phasing
+plink2 --vcf  "$input_vcf" \
+   --chr chr19 \
+   --make-pgen erase-phase \
+   --out "$output_dir"/temp_"$input_vcf_name"_chr19
+plink2 --pfile  "$output_dir"/temp_"$input_vcf_name"_chr19 \
+   --recode vcf \
+   --out "$output_dir"/temp_"$input_vcf_name"_chr19
+
 # Sort VCF
-bcftools sort $input_vcf \
-   -Oz -o "$output_dir"/temp_"$input_vcf_name"_sorted.vcf.gz
+bcftools sort "$output_dir"/temp_"$input_vcf_name"_chr19 \
+   -Oz -o "$output_dir"/temp_"$input_vcf_name"_chr19_sorted.vcf.gz
 
 # Lift over from hg38 to hg19
 CrossMap vcf \
    $crossmap_chain \
-   "$output_dir"/temp_"$input_vcf_name"_sorted.vcf.gz \
+   "$output_dir"/temp_"$input_vcf_name"_chr19_sorted.vcf.gz \
    $ref_fasta \
-   "$output_dir"/temp_"$input_vcf_name"_hg19.vcf.gz
-
-# Pass through PLINK to remove previous phasing
-plink2 --vcf  "$output_dir"/temp_"$input_vcf_name"_hg19.vcf.gz \
-   --chr chr19 \
-   --make-pgen erase-phase \
-   --out "$output_dir"/temp_plink_no_phase_"$input_vcf_name"_hg19
-
-plink2 --pfile  "$output_dir"/temp_plink_no_phase_"$input_vcf_name"_hg19 \
-   --chr chr19 \
-   --recode vcf \
-   --out "$output_dir"/temp_no_phase_"$input_vcf_name"_hg19
+   "$output_dir"/temp_"$input_vcf_name"_hg19.vcf
 
 # Sort lifted over VCF
-bcftools sort "$output_dir"/temp_no_phase_"$input_vcf_name"_hg19.vcf \
-   -Oz -o "$output_dir"/"$input_vcf_name"_hg19_sorted.vcf.gz
+bcftools sort "$output_dir"/temp_"$input_vcf_name"_hg19.vcf \
+   -Oz -o "$output_dir"/temp_"$input_vcf_name"_hg19_sorted.vcf.gz
 
 # Add AC & AN tags (required for Eagle)
 bcftools +fill-AN-AC \
-   "$output_dir"/"$input_vcf_name"_hg19_sorted.vcf.gz \
+   "$output_dir"/temp_"$input_vcf_name"_hg19_sorted.vcf.gz \
    -Ov -o "$output_dir"/"$input_vcf_name"_hg19_sorted_fill.vcf
 
 # Compress
@@ -57,4 +55,4 @@ bgzip -f "$output_dir"/"$input_vcf_name"_hg19_sorted_fill.vcf
 bcftools index "$output_dir"/"$input_vcf_name"_hg19_sorted_fill.vcf.gz
 
 # Cleanup
-# rm "$output_dir"/temp_*
+rm "$output_dir"/temp_*
